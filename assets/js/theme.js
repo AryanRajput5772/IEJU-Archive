45e344d4157a7afe1cc8eca98cf8cf417c9ca689
Aryan Rajput <144768314+AryanRajput5772@users.noreply.github.com>
2026-03-19 10:17:58 +0530

    Fixing Theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -9,6 +9,18 @@
 //   }
 // });
 
+window.addEventListener("DOMContentLoaded", function () {
+  const theme = document.getElementById("theme-style");
+  const toggle_btn = document.querySelector("#checkbox");
+
+  let savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    theme.href = "./assets/css/theme.css";
+    if (toggle_btn) toggle_btn.checked = true;
+  }
+});
+
 const toggle_btn = document.querySelector("#checkbox");
 const theme = document.getElementById("theme-style");
 
